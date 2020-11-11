e5429dee297e816d2e885b21c77d663156a98565
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-11 17:19:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,17 +22,17 @@ class Dog {
   }
 }
 
-// class Bird {
-//   constructor(name, sex) {
-//     this.name = name;
-//     this.sex =  sex;
-//   }
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex =  sex;
+  }
 
-//   speak() {
-//     if (this.sex == "male") {
-//       return `It's me! ${this.name}, the parrot!`
-//     } else {
-//       return `${this.name} says squawk!`
-//     }
-//   }
-// }
+  speak() {
+    if (this.sex == "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
+    }
+  }
+}
